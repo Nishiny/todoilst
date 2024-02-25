8f54f106ef2804c5aa74eb27910bd7b80ac706f4
Nishiny <160812882+Nishiny@users.noreply.github.com>
2024-02-25 22:32:16 +0800

    todolist update

--- todolist/src/views/HomeView.vue
+++ todolist/src/views/HomeView.vue
@@ -3,17 +3,18 @@
 </script>
 
 <template>
- <div>
-    <div class="add">
-      <input type="text" id="" class="add-text">
-      <button class="add-Todo" type="button">新增</button>
+<div class="body w-screen h-screen flex justify-center items-center bg-neutral-800">
+    <div class="w-[80rem] h-[50rem] bg-stone-400 border-4 flex flex-col">
+    <div class="add m-2 ml-96">
+      <input type="text" id="" class="add-text w-[30rem] rounded-lg text-2xl">
+      <button class="add-Todo w-20 rounded-lg text-2xl ml-8 bg-red-50" type="button">新增</button>
     </div>
-    <div class="search-btn">
-      <button class="all" type="button" data-search="all">全部</button>
-      <button class="is-todo" type="button" data-search="isTodo">已執行</button>
-      <button class="not-todo" type="button" data-search="notTodo">未執行</button>
+    <div class="search-btn m-4 text-2xl">
+      <button class="all w-20 rounded-lg text-2xl ml-5 bg-red-50" type="button" data-search="all">全部</button>
+      <button class="is-todo w-20 rounded-lg text-2xl ml-5 bg-red-50" type="button" data-search="isTodo">已執行</button>
+      <button class="not-todo w-20 rounded-lg text-2xl ml-5 bg-red-50" type="button" data-search="notTodo">未執行</button>
     </div>
-    <table class="todo">
+    <table class="todo  border-4 text-2xl">
       <thead>
         <tr>
           <th>執行</th>
@@ -25,6 +26,7 @@
 
       </tbody>
     </table>
-    <button class="enter-to-local" type="button">存進local</button>
+    <button class="enter-to-local w-32 rounded-lg text-2xl bg-red-50" type="button">存進local</button>
   </div>
+</div>
 </template>

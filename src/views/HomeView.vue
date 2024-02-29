f48dc1ae8c7d86813b340e21dede58494227654c
Nishiny <160812882+Nishiny@users.noreply.github.com>
2024-02-29 10:56:35 +0800

    更新

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,19 @@
 export default {
   data() {
     return {
+      arr:[],
+      push:{},
       
     }
   },
 
   methods: {
-
+  pushobj() {
+    this.push.id = 1; 
+  },
+  addNews() {
+    this.push.news = '';
+  }
  },
 };
 
@@ -17,8 +24,8 @@ export default {
 <div class="body w-screen h-screen flex justify-center items-center bg-neutral-800">
     <div class="w-[80rem] h-[50rem] bg-stone-400 border-4 flex flex-col">
     <div class="add m-2 ml-96">
-      <input type="text" id="" class="add-text w-[30rem] rounded-lg text-2xl">
-      <button class="add-Todo w-20 rounded-lg text-2xl ml-8 bg-red-50" type="button">新增</button>
+      <input type="text" id="" class="add-text w-[30rem] rounded-lg text-2xl" @change="addNews()">
+      <button class="add-Todo w-20 rounded-lg text-2xl ml-8 bg-red-50" @click="pushobj()" type="button">新增</button>
     </div>
     <div class="search-btn m-4 text-2xl">
       <button class="all w-20 rounded-lg text-2xl ml-5 bg-red-50" type="button" data-search="all">全部</button>
@@ -34,7 +41,11 @@ export default {
         </tr>
       </thead>
       <tbody class="data-show">
-        
+        <tr>
+          <th><input type="checkbox"></th>
+          <th>事項</th>
+          <th>功能</th>
+        </tr>
       </tbody>
     </table>
     <button class="enter-to-local w-32 rounded-lg text-2xl bg-red-50" type="button">存進local</button>
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base:'/',
+  base:'/todoilst',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
